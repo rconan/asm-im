f3928cf750cf34cc4b49d45ed29cb5b96992fabc
Rod Conan <rconan@gmto.org>
2022-09-20 22:34:31 +0000

    ASM FOR NGAO INITIAL REVISION
    use a 2% damping and the corresponding mount controller at 8kHz

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use dos_actors::{
     clients::{
-        arrow_client::{Arrow,FileFormat,MatFormat,Get},
+        arrow_client::Arrow,
+        fem::M1SegmentsAxialD,
         asm::*,
         m1::*,
         mount::{Mount, MountEncoders, MountSetPoint, MountTorques},
@@ -16,10 +17,10 @@ use fem::{
     fem_io::*,
     FEM,
 };
-use lom::{Stats, LOM};
-use std::{env, sync::Arc,path::Path};
+use std::{env, sync::Arc,path::Path,fs::File,collections::HashMap};
 use vec_box::vec_box;
-        use matio_rs::{MatFile, MatVar, Save};
+use nalgebra as na;
+use uid::UID;
 
 pub const CFD_CASES: [&str;15] = [
     "zen30az000_OS2",
@@ -38,6 +39,11 @@ pub const CFD_CASES: [&str;15] = [
     "zen30az045_CD17",
     "zen60az045_OS7",
 ];
+
+#[cfg(feature="damping0005")]
+const FEM_MODAL_DAMPING: f64 = 0.005;
+#[cfg(feature="damping002")]
+const FEM_MODAL_DAMPING: f64 = 0.02;
 
 #[derive(Default)]
 pub struct Adder {
@@ -70,29 +76,66 @@ impl Write<MCM2Lcl6F> for Adder {
     }
 }
 
+#[derive(UID)]
+#[alias(
+    name = "OSSM1Lcl",
+    client = "DiscreteModalSolver<ExponentialMatrix>",
+    traits = "Write,Size"
+)]
+enum M1RigidBodyMotions {}
+#[derive(UID)]
+#[alias(
+    name = "MCM2Lcl6D",
+    client = "DiscreteModalSolver<ExponentialMatrix>",
+    traits = "Write,Size"
+)]
+enum M2RigidBodyMotions {}
+
+#[derive(UID)]
+#[alias(
+    name = "M1SegmentsAxialD",
+    client = "DiscreteModalSolver<ExponentialMatrix>",
+    traits = "Write"
+)]
+enum M1BendingModes {}
+
+fn fig_2_mode(sid: u32) -> na::DMatrix<f64> {
+    let root_env = env::var("M1CALIBRATION").unwrap_or_else(|_| ".".to_string());
+    let root = Path::new(&root_env);
+    let fig_2_mode: Vec<f64> =
+        bincode::deserialize_from(File::open(root.join(format!("m1s{sid}fig2mode.bin"))).unwrap())
+            .unwrap();
+    if sid < 7 {
+        na::DMatrix::from_vec(162, 602, fig_2_mode)
+    } else {
+        na::DMatrix::from_vec(151, 579, fig_2_mode).insert_rows(151, 11, 0f64)
+    }
+}
+
 pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
     let sim_sampling_frequency = 8000;
-    let sim_duration = 60_usize;
+    let sim_duration = 30_usize;
     let n_step = sim_sampling_frequency * sim_duration;
+    let n_part = 5;
 
     let mut fem = FEM::from_env()?.static_from_env()?;
     let n_io = (fem.n_inputs(), fem.n_outputs());
     println!("{:}", fem);
 
-    let mut cfd_loads: Initiator<_> = (
-        windloads::CfdLoads::foh(cfd_case, sim_sampling_frequency)
+    // --------------------------------------
+    // CLIENTS
+
+    let cfd_loads_client = windloads::CfdLoads::foh(cfd_case, sim_sampling_frequency)
             .duration(sim_duration as f64)
             .mount(&mut fem, 0, None)
             .m1_segments()
             .m2_segments()
-            .build()?,
-        "CFD Wind loads",
-    )
-        .into();
+            .build()?.into_arcx();
 
     let state_space = DiscreteModalSolver::<ExponentialMatrix>::from_fem(fem)
         .sampling(sim_sampling_frequency as f64)
-        .proportional_damping(0.5 / 100.)
+        .proportional_damping(FEM_MODAL_DAMPING)
+        .use_static_gain_compensation(n_io)
         .ins::<OSSElDriveTorque>()
         .ins::<OSSAzDriveTorque>()
         .ins::<OSSRotDriveTorque>()
@@ -119,35 +162,99 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .outs::<MCM2Lcl6D>()
         .outs::<MCM2RB6D>()
         .outs::<OSSTrussIF6D>()
-        .use_static_gain_compensation(n_io)
-        .build()?;
-    println!("{state_space}");
+        .outs_with::<M1Segment1AxialD>(fig_2_mode(1))
+        .outs_with::<M1Segment2AxialD>(fig_2_mode(2))
+        .outs_with::<M1Segment3AxialD>(fig_2_mode(3))
+        .outs_with::<M1Segment4AxialD>(fig_2_mode(4))
+        .outs_with::<M1Segment5AxialD>(fig_2_mode(5))
+        .outs_with::<M1Segment6AxialD>(fig_2_mode(6))
+        .outs_with::<M1Segment7AxialD>(fig_2_mode(7))
+        .outs::<OSSM1EdgeSensors>()
+        .build()?.into_arcx();
+    //println!("{state_space}");
+
+        let signal = {
+    let signal: std::result::Result<OneSignal,_> = 
+    Signals::new(1,n_step)
+    .output_signal(0,Signal::Sigmoid{amplitude:1f64,sampling_frequency_hz:sim_sampling_frequency as f64})
+    .progress().into();
+            signal?.into_arcx()
+};
+let m1_smoother = Smooth::new().into_arcx();
+let m2_smoother = Smooth::new().into_arcx();
+let mount_smoother = Smooth::new().into_arcx();
+let adder_client = Adder::default().into_arcx();
+
+let mount_client = Mount::new().into_arcx();
+    let  m1_hardpoints_client = m1_ctrl::hp_dynamics::Controller::new().into_arcx();
+    let  m1_hp_loadcells_client =
+        m1_ctrl::hp_load_cells::Controller::new().into_arcx();
+
+    let  m1_client_segment1 =
+        m1_ctrl::actuators::segment1::Controller::new().into_arcx();
+    let  m1_client_segment2 =
+        m1_ctrl::actuators::segment2::Controller::new().into_arcx();
+    let  m1_client_segment3 =
+        m1_ctrl::actuators::segment3::Controller::new().into_arcx();
+    let  m1_client_segment4 =
+        m1_ctrl::actuators::segment4::Controller::new().into_arcx();
+    let  m1_client_segment5 =
+        m1_ctrl::actuators::segment5::Controller::new().into_arcx();
+    let  m1_client_segment6 =
+        m1_ctrl::actuators::segment6::Controller::new().into_arcx();
+    let  m1_client_segment7 =
+        m1_ctrl::actuators::segment7::Controller::new().into_arcx();
+
+            let  mount_set_point_client = Signals::new(3, n_step).into_arcx();
+let  m1rbm_set_point_client = Signals::new(42, n_step).into_arcx();
+
+    let  m2_pos_cmd_client = Signals::new(42, n_step).into_arcx();
+let  m2_positionner_client = m2_ctrl::positionner::Controller::new().into_arcx();
+
+    let  asm_cmd_client = Signals::new(21, n_step).into_arcx();
+    let  asm_inner_client = m2_ctrl::ptt_fluid_damping::Controller::new().into_arcx();
+
+        let mut meta_data: HashMap<String, String> = HashMap::new();
+    meta_data.insert(
+        "sim_sampling_frequency".to_string(),
+        format!("{sim_sampling_frequency}"),
+    );
+    meta_data.insert("sim_duration".to_string(), format!("{sim_duration}"));
+    meta_data.insert("FEM".to_string(), env::var("FEM_REPO").unwrap());
+    meta_data.insert("CFD_CASE".to_string(), cfd_case.to_string());
+
+// -----------------------------------
+// MODEL 
+
+    for part in 0..n_part {        
+
+        (*cfd_loads_client.lock().await).start_from(part*n_step/n_part);
+        (*cfd_loads_client.lock().await).stop_after((part+1)*n_step/n_part);
+
+    let mut cfd_loads: Initiator<_> = Actor::new(cfd_loads_client.clone())
+        .name("CFD Wind loads");
 
     // FEM
-    let mut fem: Actor<_> = (
-        state_space,
+    let mut fem: Actor<_> = Actor::new(
+        state_space.clone()).name(
         format!(
             "FEM w/ ASM:
         {:}",
             env::var("FEM_REPO").unwrap()
-        ),
-    )
-        .into();
+        ));
 
-    let signal: std::result::Result<OneSignal,_> = Signals::new(1,n_step).output_signal(0,Signal::Sigmoid{amplitude:1f64,sampling_frequency_hz:sim_sampling_frequency as f64}).progress().into();
     let mut sigmoid: Initiator<OneSignal,1> = 
-    (signal?,"Sigmoid").into();
-    let mut smooth_m1_loads: Actor<_> = Smooth::new().into();
-    let mut smooth_m2_loads: Actor<_> = Smooth::new().into();
-    let mut smooth_mount_loads: Actor<_> = Smooth::new().into();
-    let mut adder: Actor<_> = Adder::default().into();
+    Actor::new(signal.clone()).name("Sigmoid");
+    let mut smooth_m1_loads: Actor<_> = Actor::new(m1_smoother.clone());
+    let mut smooth_m2_loads: Actor<_> = Actor::new(m2_smoother.clone());
+    let mut smooth_mount_loads: Actor<_> = Actor::new(mount_smoother.clone());
+    let mut adder: Actor<_> = Actor::new(adder_client.clone());
 
-    let decimation = 8;
-    let logging = Arrow::builder(n_step).decimation(decimation)
-    .no_save()
-    //.file_format(FileFormat::Matlab(MatFormat::TimeBased(sim_sampling_frequency as f64)))
-    .build().into_arcx();
-    let mut sink = Terminator::<_>::new(logging.clone());
+    let decimation = 4;
+    let mut sink: Terminator<_> = Arrow::builder(n_step)
+            .metadata(meta_data.clone())
+            .filename(format!("asms-im_windloading-part{part}"))
+            .decimation(decimation).build().into();
 
     sigmoid
         .add_output()
@@ -183,33 +290,25 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .build::<CFD2021106F>()
         .into_input(&mut fem);
     // MOUNT
-    let mut mount: Actor<_> = Mount::new().into();
+    let mut mount: Actor<_> = Actor::new(mount_client.clone());
 
     const M1_RATE: usize = 80;
     assert_eq!(sim_sampling_frequency / M1_RATE, 100);
 
     // HARDPOINTS
-    let mut m1_hardpoints: Actor<_> = m1_ctrl::hp_dynamics::Controller::new().into();
+    let mut m1_hardpoints: Actor<_> = Actor::new(m1_hardpoints_client.clone());
     // LOADCELLS
-    let mut m1_hp_loadcells: Actor<_, 1, M1_RATE> =
-        m1_ctrl::hp_load_cells::Controller::new().into();
+    let mut m1_hp_loadcells: Actor<_, 1, M1_RATE> = Actor::new(m1_hp_loadcells_client.clone());
     // M1 SEGMENTS ACTUATORS
-    let mut m1_segment1: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment1::Controller::new().into();
-    let mut m1_segment2: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment2::Controller::new().into();
-    let mut m1_segment3: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment3::Controller::new().into();
-    let mut m1_segment4: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment4::Controller::new().into();
-    let mut m1_segment5: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment5::Controller::new().into();
-    let mut m1_segment6: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment6::Controller::new().into();
-    let mut m1_segment7: Actor<_, M1_RATE, 1> =
-        m1_ctrl::actuators::segment7::Controller::new().into();
+    let mut m1_segment1: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment1.clone());
+    let mut m1_segment2: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment2.clone());
+    let mut m1_segment3: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment3.clone());
+    let mut m1_segment4: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment4.clone());
+    let mut m1_segment5: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment5.clone());
+    let mut m1_segment6: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment6.clone());
+    let mut m1_segment7: Actor<_, M1_RATE, 1> = Actor::new(m1_client_segment7.clone());
 
-    let mut mount_set_point: Initiator<_> = (Signals::new(3, n_step), "Mount Set Point").into();
+    let mut mount_set_point: Initiator<_> = Actor::new(mount_set_point_client.clone()).name("Mount Set Point");
     mount_set_point
         .add_output()
         .build::<MountSetPoint>()
@@ -219,7 +318,7 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .build::<MountTorques>()
         .into_input(&mut fem);
 
-    let mut m1rbm_set_point: Initiator<_> = (Signals::new(42, n_step), "M1 RBM Set Point").into();
+    let mut m1rbm_set_point: Initiator<_> = Actor::new(m1rbm_set_point_client.clone()).name("M1 RBM Set Point");
     m1rbm_set_point
         .add_output()
         .build::<M1RBMcmd>()
@@ -297,11 +396,9 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .into_input(&mut fem);
 
     // M2 POSITIONER COMMAND
-    let mut m2_pos_cmd: Initiator<_> =
-        (Signals::new(42, n_step), "M2 Positionner Set Point").into();
+    let mut m2_pos_cmd: Initiator<_> = Actor::new(m2_pos_cmd_client.clone()).name("M2 Positionner Set Point");
     // FSM POSITIONNER
-    let mut m2_positionner: Actor<_> =
-        (m2_ctrl::positionner::Controller::new(), "M2 Positionner").into();
+    let mut m2_positionner: Actor<_> = Actor::new(m2_positionner_client.clone()).name("M2 Positionner");
     m2_pos_cmd
         .add_output()
         .build::<M2poscmd>()
@@ -311,14 +408,12 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .build::<MCM2SmHexF>()
         .into_input(&mut fem);
     // ASM SET POINT
-    let mut asm_cmd: Initiator<_> = (Signals::new(21, n_step), "ASMS Set Point").into();
+    let mut asm_cmd: Initiator<_> = Actor::new(asm_cmd_client.clone()).name("ASMS Set Point");
     // ASM INNER CONTROLLER
-    let mut asm_inner: Actor<_> = (
-        m2_ctrl::ptt_fluid_damping::Controller::new(),
+    let mut asm_inner: Actor<_> = Actor::new(asm_inner_client.clone()).name(
         "ASMS
         piston,tip,tilt & fluid damping",
-    )
-        .into();
+    );
     asm_cmd
         .add_output()
         .build::<Rrbfs>()
@@ -335,28 +430,22 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
 
     fem.add_output()
         .bootstrap()
-        .multiplex(2)
         .build::<MountEncoders>()
-        .into_input(&mut mount)
-        .logn(&mut sink,14).await
-        .confirm()?;
+        .into_input(&mut mount);
     fem.add_output()
         .bootstrap()
         .build::<OSSHardpointD>()
         .into_input(&mut m1_hp_loadcells);
     fem.add_output()
         .bootstrap()
-        .build::<OSSM1Lcl>()
+        .unbounded()
+        .build::<M1RigidBodyMotions>()
         .log(&mut sink)
         .await;    
     fem.add_output()
-        .bootstrap()
-        .build::<OSSTrussIF6D>()
-        .logn(&mut sink,18)
-        .await;
-    fem.add_output()
         .multiplex(2)
         .bootstrap()
+        .unbounded()
         .build::<MCM2Lcl6D>()
         .into_input(&mut asm_inner)
         .log(&mut sink)
@@ -371,6 +460,18 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
         .build::<MCM2RB6D>()
         .into_input(&mut asm_inner)
         .confirm()?;
+    fem.add_output()
+        .bootstrap()
+        .unbounded()
+        .build::<M1BendingModes>()
+        .logn(&mut sink, 162 * 7)
+        .await;
+    fem.add_output()
+        .bootstrap()
+        .unbounded()
+        .build::<OSSM1EdgeSensors>()
+        .logn(&mut sink, 288)
+        .await;
 
     Model::new(vec_box![
         mount_set_point,
@@ -405,33 +506,7 @@ pub async fn model(cfd_case: &str) -> anyhow::Result<()> {
     .wait()
     .await?;
 
-    let logger = &mut *logging.lock().await;
-    let m1_rbm: Vec<Vec<f64>> = logger.get("OSSM1Lcl")?;
-    let m2_rbm: Vec<Vec<f64>> = logger.get("MCM2Lcl6D")?;
-    let truss_if: Vec<Vec<f64>> = logger.get("OSSTrussIF6D")?;
-
-    let n_sample = m1_rbm.len();
-    let n_data = m1_rbm[0].len() + m2_rbm[0].len() + truss_if[0].len();
-
-    let  data: Vec<f64> = m1_rbm.into_iter()
-    .zip(m2_rbm.into_iter())
-    .zip(truss_if.into_iter())
-    .flat_map(|((a,b),c)| a.into_iter().chain(b.into_iter()).chain(c.into_iter()).collect::<Vec<f64>>()).collect();
-    let tau = ((sim_sampling_frequency/decimation) as f64).recip();
-    let time: Vec<_> = (0..n_sample).map(|i| tau * i as f64).collect();
-
-    let root = Path::new(cfd_case).join("windmotions").with_extension("mat");
-    let mat_file = MatFile::save(&root)?;
-    mat_file.write(MatVar::<Vec<f64>>::new("tt",time.as_slice())?);
-    mat_file.write(MatVar::<Vec<f64>>::array("yt",data.as_slice(),(n_data,n_sample))?);
-
-/*     let lom = LOM::builder()
-        .rigid_body_motions_record((*logging.lock().await).record()?,Some("OSSM1Lcl"),Some("MCM2Lcl6D"))?
-        .build()?;
-    let tiptilt = lom.tiptilt_mas();
-    let n_sample = 1000;
-    let tt = tiptilt.std(Some(n_sample));
-    println!("TT STD.: {:.3?}mas", tt); */
+    }
 
 
     Ok(())
